509ee736a43199a6edfcd4ad7eaea658baa74849
Carrotzpc <zhang.pc3@gmail.com>
2024-01-26 19:41:20 +0800

    chore: fix proxy and url parse issue

--- server.mjs
+++ server.mjs
@@ -5,28 +5,38 @@ import express from 'express';
 import { createProxyMiddleware } from 'http-proxy-middleware';
 import url from 'url';
 
-const port = parseInt(process.env.PORT, 10) || 3000;
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
-const bff = '/bff';
-const api = '/kubeagi-apis';
+async function bootstrap() {
+  const port = Number.parseInt(process.env.PORT, 10) || 3000;
+  const dev = process.env.NODE_ENV !== 'production';
+  const app = next({
+    dev,
+    customServer: true,
+  });
+  const handle = app.getRequestHandler();
 
-app.prepare().then(() => {
-  const oidcUrlObj = url.parse(process.env.OIDC_SERVER_URL);
-  const _url = `${oidcUrlObj.protocol}//${oidcUrlObj.host}${oidcUrlObj.port ? `:${oidcUrlObj.port}` : ''}`;
+  await app.prepare();
+
+  const oidcServerUrl = process.env.OIDC_SERVER_URL;
+  if (!oidcServerUrl) {
+    console.warn('The env OIDC_SERVER_URL must be configured!')
+    return;
+  }
+  const oidcUrlObj = url.parse(oidcServerUrl);
+  const _url = `${oidcUrlObj.protocol}//${oidcUrlObj.host}`;
   const server = express();
-  // 保持代理中间件配置
+
+  // 代理中间件配置
+  const bff = '/bff';
+  const api = '/kubeagi-apis';
   server.use(bff, createProxyMiddleware({
-    target: _url + bff ,
+    target: _url ,
     changeOrigin: true,
     secure: false, // 关闭 SSL 证书验证
   }));
-
   server.use(api, createProxyMiddleware({
-    target: _url + api,
+    target: _url,
     changeOrigin: true,
-    secure: false, // 关闭 SSL 证书验证
+    secure: false,
   }));
 
   // 处理其他所有请求
@@ -42,13 +52,17 @@ app.prepare().then(() => {
       cert: fs.readFileSync('./localhost.pem'),
     }, server).listen(port, err => {
       if (err) throw err;
+      // eslint-disable-next-line no-console
       console.log(`> Ready on https://localhost:${port}`);
     });
   } else {
     // 在开发环境下使用 HTTP
     server.listen(port, err => {
       if (err) throw err;
+      // eslint-disable-next-line no-console
       console.log(`> Ready on http://localhost:${port}`);
     });
   }
-});
+}
+
+bootstrap();
