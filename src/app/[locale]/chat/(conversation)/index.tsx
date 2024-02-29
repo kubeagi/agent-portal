6ea6e414293ab0b13aa9dd1762f928931f2e6c52
zggmd <shangzhisong@gmail.com>
2024-02-29 15:43:06 +0800

    fix: chat darkmode issue

--- src/app/[locale]/chat/(conversation)/index.tsx
+++ src/app/[locale]/chat/(conversation)/index.tsx
@@ -5,6 +5,9 @@ import { createStyles } from 'antd-style';
 import { useParams, useSearchParams } from 'next/navigation';
 import React from 'react';
 import { Flexbox } from 'react-layout-kit';
+import { useSelector } from 'react-redux';
+
+import useDark from '@/app/[locale]/chat/(conversation)/useDark';
 
 export const useStyles = createStyles(({ token }) => ({
   conversationWrapper: {
@@ -21,6 +24,11 @@ const Conversation = React.memo<ConversationProps>(() => {
   const { styles } = useStyles();
   const params = useParams();
   const searchParams = useSearchParams();
+  const theme = useSelector((store: any) => store.theme);
+  const isDark =
+    theme === 'dark' ||
+    (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches);
+  useDark(isDark);
   return (
     <>
       <Flexbox className={styles.conversationWrapper} flex={1} horizontal>
@@ -29,7 +37,7 @@ const Conversation = React.memo<ConversationProps>(() => {
           appNamespace={searchParams.get('appNamespace') || ''}
           conversationId={params.id as string}
           gpts
-          isDark={false}
+          isDark={isDark}
         />
       </Flexbox>
     </>
