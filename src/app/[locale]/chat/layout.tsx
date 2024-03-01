1ad8931a9709488ad9ca7a38d7715b52ff32b8f0
Vsion <442153598@qq.com>
2024-03-01 17:18:08 +0800

    feat: chats actions

---
+++ src/app/[locale]/chat/layout.tsx
@@ -0,0 +1,12 @@
+import '@yuntijs/chat/dist/umd/index.min.css';
+import { ReactNode } from 'react';
+
+type Props = {
+  children: ReactNode;
+};
+
+// Since we have a `not-found.tsx` page on the root, a layout file
+// is required, even if it's just passing children through.
+export default function RootLayout({ children }: Props) {
+  return children;
+}
