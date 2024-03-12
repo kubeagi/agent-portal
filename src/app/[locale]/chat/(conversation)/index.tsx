226489ac0693e58830024a214747ffab7a2590a0
zggmd <shangzhisong@gmail.com>
2024-03-12 16:05:11 +0800

    chore: bump @yuntijs/chat@0.1.0-beta.6

--- src/app/[locale]/chat/(conversation)/index.tsx
+++ src/app/[locale]/chat/(conversation)/index.tsx
@@ -6,8 +6,6 @@ import React from 'react';
 import { Flexbox } from 'react-layout-kit';
 import { useSelector } from 'react-redux';
 
-import useDark from '@/app/[locale]/chat/(conversation)/useDark';
-
 export const useStyles = createStyles(({ token }) => ({
   conversationWrapper: {
     position: 'relative',
@@ -29,7 +27,6 @@ const Conversation = React.memo<ConversationProps>(({ params, searchParams }) =>
   const isDark =
     theme === 'dark' ||
     (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches);
-  useDark(isDark);
 
   return (
     <>
